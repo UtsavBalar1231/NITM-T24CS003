e7da2c685d4ab9d637472d88e7c03585f1dc7111
utsavbalar1231@gmail.com <utsavbalar1231@gmail.com>
2024-08-19 17:32:30 +0530

    fixes

--- DIP_Lab/p1_opencv/p1.py
+++ DIP_Lab/p1_opencv/p1.py
@@ -1,14 +1,16 @@
 import cv2 as cv
+import matplotlib.pyplot as plt
 
 def show_image_stats(img):
     print("[row, colomns, channels]: ", img.shape)
-    print("[size, dtype]: ", img.size, img.dtype)
+    print(f"[size {img.size} bytes, dtype {img.dtype}]:")
 
 def resize_img(img):
     print("Image Stats after resize")
     img = cv.resize(img, tuple(i/2 for i in img.size))
     show_image_stats(img)
-    plt.imshow("Resized Image (50% resolution)", img)
+    plt.title("Resized Image (50% resolution)")
+    plt.imshow(img)
     plt.show()
 
 def open_image(image) -> None:
@@ -16,9 +18,10 @@ def open_image(image) -> None:
     assert img is not None, "Failed to load nyaa_cat.png"
     print("Original Image Stats")
     show_image_stats(img)
-    plt.imshow("Original Image", img)
+    plt.title("Original Image")
+    plt.imshow(img)
     plt.show()
 
-if __main__:
+if __name == '__main__':
     image = "./nyaa_cat.png"
     open_image(image)
